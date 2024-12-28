6cba81fd078ac1c69aebcd2273ba37e8347c1bcb
Calvin Kinateder <calvinkinateder@gmail.com>
2024-12-28 03:44:40 +0000

    initial tsetup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ libTM = Extension('libTM',
 
 setup(
    name='pyTsetlinMachineParallel',
-   version='0.2.1',
+   version='0.3.1',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
    url='https://github.com/cair/pyTsetlinMachineParallel/',

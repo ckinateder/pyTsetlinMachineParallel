cd2775a73b079d35c38ea77b1ffa45bf508fbc5d
Calvin Kinateder <calvinkinateder@gmail.com>
2025-02-23 04:05:03 +0000

    Some tweaks

--- examples/distill.py
+++ examples/distill.py
@@ -3,15 +3,15 @@ import numpy as np
 from time import time
 from pickle import dump, load
 
-from keras.datasets import mnist, fashion_mnist
+from keras.datasets import mnist
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
-teacher_params = (400, 10, 10)
-student_params = (100, 10, 10)
+teacher_params = (1000, 10, 10)
+student_params = (50, 10, 10)
 
 teacher = MultiClassTsetlinMachine(*teacher_params)
 teacher_epochs = 20
